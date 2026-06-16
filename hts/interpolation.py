c794020709a62e714ee71b4064754fa9218e62ae
Peppe <piccininnig5@gmail.com>
2026-06-16 23:54:26 +0200

    linear interpolation implemented

--- hts/interpolation.py
+++ hts/interpolation.py
@@ -1,23 +1,24 @@
 import numpy as np
 
+
 def sample_and_hold(t_in, y_in, t_out):
-    t_in  = np.asarray(t_in,  dtype=float)
-    y_in  = np.asarray(y_in,  dtype=float)
+    t_in = np.asarray(t_in, dtype=float)
+    y_in = np.asarray(y_in, dtype=float)
     t_out = np.asarray(t_out, dtype=float)
 
     idx = np.searchsorted(t_in, t_out, side="right")
     idx_clamped = np.clip(idx - 1, 0, len(y_in) - 1)  # avoid negative indexes or IndexOutOfBound
-    out_val = y_in[idx_clamped]                         # vectorized indexes
+    out_val = y_in[idx_clamped]  # vectorized indexes
 
-    out_val[t_out < t_in[0]]  = np.nan                 # left threshold
-    out_val[t_out >= t_in[-1]] = y_in[-1]              # right threshold
+    out_val[t_out < t_in[0]] = np.nan  # left threshold
+    out_val[t_out >= t_in[-1]] = y_in[-1]  # right threshold
 
     return out_val
 
 
 def nearest_neighbour(t_in, y_in, t_out):
-    t_in  = np.asarray(t_in,  dtype=float)
-    y_in  = np.asarray(y_in,  dtype=float)
+    t_in = np.asarray(t_in, dtype=float)
+    y_in = np.asarray(y_in, dtype=float)
     t_out = np.asarray(t_out, dtype=float)
 
     out_val = np.full(len(t_out), np.nan)
@@ -29,14 +30,51 @@ def nearest_neighbour(t_in, y_in, t_out):
         if current_time < t_in[0] or current_time > t_in[-1]:
             pass  # already NaN
         else:
-            i_right = idx[i]                # indice campione a destra
-            i_left  = idx[i] - 1            # indice campione a sinistra
-            mid     = (t_in[i_right] + t_in[i_left])/2  # midpoint tra t_in[i_left] e t_in[i_right]
-            
+            i_right = idx[i]  # indice campione a destra
+            i_left = idx[i] - 1  # indice campione a sinistra
+            mid = (t_in[i_right] + t_in[i_left]) / 2  # midpoint tra t_in[i_left] e t_in[i_right]
+
             if current_time < mid:
-                out_val[i] = y_in[i_left]   # prendi il campione a sinistra
+                out_val[i] = y_in[i_left]  # prendi il campione a sinistra
             else:
                 out_val[i] = y_in[i_right]  # prendi il campione a destra
 
     return out_val
-        
+
+
+def liner_interp(t_in, y_in, t_out):
+    t_in = np.asarray(t_in, dtype=float)
+    y_in = np.asarray(y_in, dtype=float)
+    t_out = np.asarray(t_out, dtype=float)
+
+    out_val = np.full(len(t_out), np.nan)
+    idx = np.searchsorted(t_in, t_out, side="right")
+
+    for i in range(len(t_out)):
+        current_time = t_out[i]
+
+        if current_time < t_in[0]:
+            pass
+        else:
+            i_right = idx[i]
+            i_left = idx[i] - 1
+
+            # caso esatto sull'ultimo campione
+            if i_right == len(t_in):
+                out_val[i] = y_in[-1]
+                continue
+
+            t_left = t_in[i_left]
+            t_right = t_in[i_right]
+
+            y_left = y_in[i_left]
+            y_right = y_in[i_right]
+
+            # coefficiente di interpolazione
+            alpha = (current_time - t_left) / (t_right - t_left)
+
+            out_val[i] = y_left + alpha * (y_right - y_left)
+
+        return out_val
+
+    return out_val
